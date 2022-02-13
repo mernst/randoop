818d2d4fee3dd81be20c1aa2427f2d66fa088bfd
Michael Ernst <mernst@cs.washington.edu>
2022-02-13 12:00:23 -0800

    More output to stdout

--- scripts/test-nonSystemTest-temp.sh
+++ scripts/test-nonSystemTest-temp.sh
@@ -23,3 +23,5 @@ sleep 3 # give xvfb some time to start
 
 # `gradle build` == `gradle check assemble`.
 ./gradlew --info --stacktrace test --tests '*testMultipleThrows'
+
+cat /__w/1/s/agent/replacecall/build/test-results/test/*
